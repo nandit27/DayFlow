e3eccf50c65e45db0f58539ee6a9edf23587ee53
nandit27 <nanditkalaria27@gmail.com>
2026-01-03 09:04:40 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import './App.css'
+import { Button } from './components/ui/button'
+
+function App() {
+
+  return (
+    <>
+      <h1>This is a custom Vite+React+shadcn template</h1>
+      <Button>This is a shadcn button </Button>
+    </>
+  )
+}
+
+export default App
